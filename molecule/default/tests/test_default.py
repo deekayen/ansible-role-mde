49a2133719042816b45896babb7e4ff01e8e5bc6
David Norman <deekayen@deekayen.net>
2021-02-09 00:15:13 -0500

    Remove some testinfra tests that probably aren't needed.

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -13,27 +13,7 @@ def test_mdatp_etc(host):
     assert host.file("/etc/opt/microsoft/mdatp/mdatp_onboard.json").exists
 
 
-def test_mdatp_logs(host):
-    for filename in (
-        ("/var/log/microsoft/mdatp_install.log"),
-    ):
-        log = host.file(filename)
-        assert log.exists
-
-
 def test_mdatp_service(host):
     service = host.service("mdatp")
 
     assert service.is_enabled
-
-
-def test_mdatp_connectivity(host):
-    cmd = host.run("mdatp connectivity test")
-
-    assert cmd.succeeded
-
-
-def test_mdatp_health(host):
-    cmd = host.run("mdatp health")
-
-    assert cmd.succeeded
